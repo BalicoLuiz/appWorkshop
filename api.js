862dbb5bc298862f8fdadfe0e49aab52918e8282
balicoLuiz <luiz.balico@gmail.com>
2024-12-18 09:07:58 -0300

    alteração para ser apis

--- api.js
+++ api.js
@@ -9,17 +9,17 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/funcionarios', async (req, res) => {
-        const { nome } = req.query;
-        if (!nome) {
-            return res.status(400).send('O parâmetro "nome" é obrigatório');
+app.get('/proxys', async (req, res) => {
+        const { nome_api } = req.query;
+        if (!nome_api) {
+            return res.status(400).send('O parâmetro "nome_api" é obrigatório');
           }
         const query = 'SELECT * FROM funcionarios WHERE nome ILIKE $1';
-        const values = [`%${nome}%`];
+        const values = [`%${nome_api}%`];
         client.query(query, values)
         .then(result => {
           if (result.rows.length === 0) {
-            return res.status(404).json({ message: 'funcionario não encontrado.' });
+            return res.status(404).json({ message: 'nome_api não encontrado.' });
           }
           res.setHeader('Content-Type', 'application/json');
           let response = result.rows
@@ -31,20 +31,20 @@ app.get('/funcionarios', async (req, res) => {
         });
     });
         // Rota de exemplo (POST)
-        app.post('/funcionarios', (req, res) => {
-            const { nome, cargo, email } = req.body; // Pegando os dados do corpo da requisição
+        app.post('/proxys', (req, res) => {
+            const { nome_api, metodos } = req.body; // Pegando os dados do corpo da requisição
 
   
             // Consulta SQL para inserir os dados na tabela
-            const query = 'INSERT INTO funcionarios (nome, cargo, email) VALUES ($1, $2, $3) RETURNING id';
-            const values = [nome, cargo, email];
+            const query = 'INSERT INTO funcionarios (nome_api, metodos) VALUES ($1, $2) RETURNING id';
+            const values = [nome_api, metodos];
             client.query(query, values)
             .then(() => {
-              res.status(201).json({ message: 'Usuário inserido com sucesso!' });
+              res.status(201).json({ message: 'API inserida com sucesso!' });
             })
             .catch((err) => {
               console.error('Erro ao inserir dados:', err);
-              res.status(422).json({ message: 'Erro ao inserir usuário no banco de dados' });
+              res.status(422).json({ message: 'Erro ao inserir API no banco de dados' });
             });
         });
         
