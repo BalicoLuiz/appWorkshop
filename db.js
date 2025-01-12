26e7d46c3201804e5a6aa12efac90185ed81f2e0
balicoLuiz <luiz.balico@gmail.com>
2025-01-12 18:49:51 -0300

    ajuste

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Client } = require('pg');
 
 // Configuração de conexão com o PostgreSQL
 const client = new Client({
-  user: 'spreadapplicationworkshop_user',         // Seu usuário PostgreSQL
-  host: 'dpg-ctc2vd52ng1s73btafkg-a.oregon-postgres.render.com',       // O host do seu banco de dados (ex: 'localhost' ou IP externo)
-  database: 'spreadapplicationworkshop',       // O nome do banco de dados
-  password: 'WUe0HSoUXYHFNbyk2k0v4mtDZiMxZIII',       // Sua senha de acesso ao banco
-  port: 5432,                  // Porta padrão do PostgreSQL (pode ser diferente)
+  user: 'postgres',         // Seu usuário PostgreSQL
+  host: 'junction.proxy.rlwy.net',       // O host do seu banco de dados (ex: 'localhost' ou IP externo)
+  database: 'postgres',       // O nome do banco de dados
+  password: 'cqolvohXCcKxEuxtoJxCzqpNCdkwRdpq',       // Sua senha de acesso ao banco
+  port: 30520,                  // Porta padrão do PostgreSQL (pode ser diferente)
   ssl: {
     rejectUnauthorized: false, // Aceitar certificados não verificados (útil para servidores de nuvem como Render)
   },
