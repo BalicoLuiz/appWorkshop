c5d319cc3f818607bbc97a33c7ea0fb91e221977
balicoLuiz <luiz.balico@gmail.com>
2025-01-12 19:05:35 -0300

    ajuste db

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const { Client } = require('pg');
 const client = new Client({
   user: 'postgres',         // Seu usuário PostgreSQL
   host: 'junction.proxy.rlwy.net',       // O host do seu banco de dados (ex: 'localhost' ou IP externo)
-  database: 'postgres',       // O nome do banco de dados
+  database: 'railway',       // O nome do banco de dados
   password: 'cqolvohXCcKxEuxtoJxCzqpNCdkwRdpq',       // Sua senha de acesso ao banco
   port: 30520,                  // Porta padrão do PostgreSQL (pode ser diferente)
   ssl: {
